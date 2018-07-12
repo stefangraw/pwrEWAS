c9b7beda9f4630849f7ad0f323deb3987888afdc
stefangraw <39307180+stefangraw@users.noreply.github.com>
2018-07-12 13:00:14 -0500

    Reworked reference data sets
    Cleaned up code to obtain reference data from GEO. Some data sets used to much data to calculate mean and variance.

--- R/data.R
+++ R/data.R
@@ -1,177 +1,77 @@
-#' Prices of 50,000 round cut diamonds.
-#'
-#' A dataset containing the prices and other attributes of almost 54,000
-#' diamonds. The variables are as follows:
-#'
-#' \itemize{
-#'   \item price. price in US dollars (\$326--\$18,823)
-#'   \item carat. weight of the diamond (0.2--5.01)
-#'   ...
-#' }
-#'
-#' @format A data frame with 53940 rows and 10 variables
-#' @source \url{http://www.diamondse.info/}
-"Adult_PBMC"
-#> [1] "Adult_PBMC"
-
-
-#' Prices of 50,000 round cut diamonds.
-#'
-#' A dataset containing the prices and other attributes of almost 54,000
-#' diamonds. The variables are as follows:
-#'
-#' \itemize{
-#'   \item price. price in US dollars (\$326--\$18,823)
-#'   \item carat. weight of the diamond (0.2--5.01)
-#'   ...
-#' }
-#'
-#' @format A data frame with 53940 rows and 10 variables
-#' @source \url{http://www.diamondse.info/}
-"Blood_5yrOlds"
-#> [1] "Blood_5yrOlds"
-
-#' Prices of 50,000 round cut diamonds.
-#'
-#' A dataset containing the prices and other attributes of almost 54,000
-#' diamonds. The variables are as follows:
-#'
-#' \itemize{
-#'   \item price. price in US dollars (\$326--\$18,823)
-#'   \item carat. weight of the diamond (0.2--5.01)
-#'   ...
-#' }
-#'
-#' @format A data frame with 53940 rows and 10 variables
-#' @source \url{http://www.diamondse.info/}
-"BloodNewborns"
-#> [1] "BloodNewborns"
-
-#' Prices of 50,000 round cut diamonds.
-#'
-#' A dataset containing the prices and other attributes of almost 54,000
-#' diamonds. The variables are as follows:
-#'
-#' \itemize{
-#'   \item price. price in US dollars (\$326--\$18,823)
-#'   \item carat. weight of the diamond (0.2--5.01)
-#'   ...
-#' }
-#'
-#' @format A data frame with 53940 rows and 10 variables
-#' @source \url{http://www.diamondse.info/}
-"Colon"
-#> [1] "Colon"
-
-#' Prices of 50,000 round cut diamonds.
-#'
-#' A dataset containing the prices and other attributes of almost 54,000
-#' diamonds. The variables are as follows:
-#'
-#' \itemize{
-#'   \item price. price in US dollars (\$326--\$18,823)
-#'   \item carat. weight of the diamond (0.2--5.01)
-#'   ...
-#' }
-#'
-#' @format A data frame with 53940 rows and 10 variables
-#' @source \url{http://www.diamondse.info/}
-"CordBlood_PBMC"
-#> [1] "CordBlood_PBMC"
-
-#' Prices of 50,000 round cut diamonds.
-#'
-#' A dataset containing the prices and other attributes of almost 54,000
-#' diamonds. The variables are as follows:
-#'
-#' \itemize{
-#'   \item price. price in US dollars (\$326--\$18,823)
-#'   \item carat. weight of the diamond (0.2--5.01)
-#'   ...
-#' }
-#'
-#' @format A data frame with 53940 rows and 10 variables
-#' @source \url{http://www.diamondse.info/}
-"CordBlood_wholeBlood"
-#> [1] "CordBlood_wholeBlood"
-
-#' Prices of 50,000 round cut diamonds.
-#'
-#' A dataset containing the prices and other attributes of almost 54,000
-#' diamonds. The variables are as follows:
-#'
-#' \itemize{
-#'   \item price. price in US dollars (\$326--\$18,823)
-#'   \item carat. weight of the diamond (0.2--5.01)
-#'   ...
-#' }
-#'
-#' @format A data frame with 53940 rows and 10 variables
-#' @source \url{http://www.diamondse.info/}
-"Liver"
-#> [1] "Liver"
-
-#' Prices of 50,000 round cut diamonds.
-#'
-#' A dataset containing the prices and other attributes of almost 54,000
-#' diamonds. The variables are as follows:
-#'
-#' \itemize{
-#'   \item price. price in US dollars (\$326--\$18,823)
-#'   \item carat. weight of the diamond (0.2--5.01)
-#'   ...
-#' }
-#'
-#' @format A data frame with 53940 rows and 10 variables
-#' @source \url{http://www.diamondse.info/}
-"Lymphoma"
-#> [1] "Lymphoma"
-
-#' Prices of 50,000 round cut diamonds.
-#'
-#' A dataset containing the prices and other attributes of almost 54,000
-#' diamonds. The variables are as follows:
-#'
-#' \itemize{
-#'   \item price. price in US dollars (\$326--\$18,823)
-#'   \item carat. weight of the diamond (0.2--5.01)
-#'   ...
-#' }
-#'
-#' @format A data frame with 53940 rows and 10 variables
-#' @source \url{http://www.diamondse.info/}
-"PeripheralLeukocytes"
-#> [1] "PeripheralLeukocytes"
-
-#' Prices of 50,000 round cut diamonds.
-#'
-#' A dataset containing the prices and other attributes of almost 54,000
-#' diamonds. The variables are as follows:
-#'
-#' \itemize{
-#'   \item price. price in US dollars (\$326--\$18,823)
-#'   \item carat. weight of the diamond (0.2--5.01)
-#'   ...
-#' }
-#'
-#' @format A data frame with 53940 rows and 10 variables
-#' @source \url{http://www.diamondse.info/}
-"Placenta"
-#> [1] "Placenta"
-
-#' Prices of 50,000 round cut diamonds.
-#'
-#' A dataset containing the prices and other attributes of almost 54,000
-#' diamonds. The variables are as follows:
-#'
-#' \itemize{
-#'   \item price. price in US dollars (\$326--\$18,823)
-#'   \item carat. weight of the diamond (0.2--5.01)
-#'   ...
-#' }
-#'
-#' @format A data frame with 53940 rows and 10 variables
-#' @source \url{http://www.diamondse.info/}
+#' @title Saliva (GSE92767)
+#' @description Methylation data for GSE92767 was obtained from the genomics data repository GEO. The beta-values for all 54 patients were used to calculate CpG-specific mean and variance for 485575 CpGs.
+#' @format A data frame with 2 columuns, mean (mu) and variance (var), and 485575 rows for CpGs. 
+#' @source \url{https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE92767}
 "Saliva"
-#> [1] "Saliva"
+
+
+#' @title Lymphoma (GSE42372)
+#' @description Methylation data for GSE42372 was obtained from the genomics data repository GEO. The beta-values for 20/31 patients (disease state: non-HIV lymphoma) were used to calculate CpG-specific mean and variance for 485577 CpGs.
+#' @format A data frame with 2 columuns, mean (mu) and variance (var), and 485577 rows for CpGs. 
+#' @source \url{https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE42372}
+"Lymphoma"
+
+
+#' @title Placenta (GSE62733)
+#' @description Methylation data for GSE62733 was obtained from the genomics data repository GEO. The beta-values for all 33 patients were used to calculate CpG-specific mean and variance for 485577 CpGs.
+#' @format A data frame with 2 columuns, mean (mu) and variance (var), and 485577 rows for CpGs. 
+#' @source \url{https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE62733}
+"Placenta"
+
+
+#' @title Liver (GSE61258)
+#' @description Methylation data for GSE61258 was obtained from the genomics data repository GEO. The beta-values for 26/79 patients (diseasestatus: Control) were used to calculate CpG-specific mean and variance for 485577 CpGs.
+#' @format A data frame with 2 columuns, mean (mu) and variance (var), and 485577 rows for CpGs. 
+#' @source \url{https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE61258}
+"Liver"
+
+
+#' @title Colon (GSE77718)
+#' @description Methylation data for GSE77718 was obtained from the genomics data repository GEO. The beta-values for 96/192 patients (disease state: Normal) were used to calculate CpG-specific mean and variance for 371377 CpGs.
+#' @format A data frame with 2 columuns, mean (mu) and variance (var), and 371377 rows for CpGs. 
+#' @source \url{https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE77718}
+"Colon"
+
+
+#' @title Blood (Adults) - Peripheral Leukocytes [GSE42861]
+#' @description Methylation data for GSE42861 was obtained from the genomics data repository GEO. The beta-values for 335/689 patients (subject: Normal) were used to calculate CpG-specific mean and variance for 485577 CpGs.
+#' @format A data frame with 2 columuns, mean (mu) and variance (var), and 485577 rows for CpGs. 
+#' @source \url{https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE42861}
+"Blood_adult"
+
+
+#' @title Blood 5 year olds (GSE83334)
+#' @description Methylation data for GSE83334 was obtained from the genomics data repository GEO. The beta-values for 15/30 patients (age: 5 years) were used to calculate CpG-specific mean and variance for 485512 CpGs.
+#' @format A data frame with 2 columuns, mean (mu) and variance (var), and 485512 rows for CpGs. 
+#' @source \url{https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE83334}
+"Blood_5yrOlds"
+
+
+#' @title Blood newborns (GSE82273)
+#' @description Methylation data for GSE82273 was obtained from the genomics data repository GEO. The beta-values for all 889 patients were used to calculate CpG-specific mean and variance for 485577 CpGs.
+#' @format A data frame with 2 columuns, mean (mu) and variance (var), and 485577 rows for CpGs. 
+#' @source \url{https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE82273}
+"BloodNewborns"
+
+
+#' @title Cord-blood (whole blood) [GSE69176]
+#' @description Methylation data for GSE69176 was obtained from the genomics data repository GEO. The beta-values for all 152 patients were used to calculate CpG-specific mean and variance for 473673 CpGs.
+#' @format A data frame with 2 columuns, mean (mu) and variance (var), and 473673 rows for CpGs. 
+#' @source \url{https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE69176}
+"CordBlood_wholeBlood"
+
+
+#' @title Cord-blood (PBMC) [GSE110128]
+#' @description Methylation data for GSE110128 was obtained from the genomics data repository GEO. The beta-values for 60/233 patients (cord blood and Control) were used to calculate CpG-specific mean and variance for 485512 CpGs.
+#' @format A data frame with 2 columuns, mean (mu) and variance (var), and 485512 rows for CpGs. 
+#' @source \url{https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE110128}
+"CordBlood_PBMC"
+
+
+#' @title Adult (PBMC) [GSE67170]
+#' @description Methylation data for GSE67170 was obtained from the genomics data repository GEO. The beta-values for 20/89 patients (disease state: control) were used to calculate CpG-specific mean and variance for 485574 CpGs.
+#' @format A data frame with 2 columuns, mean (mu) and variance (var), and 485574 rows for CpGs. 
+#' @source \url{https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE67170}
+"Adult_PBMC"
+
 
